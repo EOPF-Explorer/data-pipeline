850a3a106340a285d9a9d94c72b9cdccc84d33af
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2026-01-15 14:02:23 +0100

    fix: update source URL and collection name in STAC item submission script; change eopf-geozarr dependency to a specific branch

--- operator-tools/submit_test_workflow_wh.py
+++ operator-tools/submit_test_workflow_wh.py
@@ -5,8 +5,9 @@ import requests
 
 # Test STAC item submission
 payload = {
-    "source_url": "https://stac.core.eopf.eodc.eu/collections/sentinel-2-l2a/items/S2C_MSIL2A_20251117T090251_N0511_R007_T35SMA_20251117T124014",
-    "collection": "sentinel-2-l2a-dp-test",
+    # "source_url": "https://stac.core.eopf.eodc.eu/collections/sentinel-2-l2a/items/S2C_MSIL2A_20251117T090251_N0511_R007_T35SMA_20251117T124014",
+    "source_url": "https://s3.explorer.eopf.copernicus.eu/esa-zarr-sentinel-explorer-fra/cpm-manual/S2A_MSIL2A_20210917T115221_N0500_R123_T28RBS_20230110T165456.json",
+    "collection": "sentinel-2-l2a-staging",
     "action": "convert-v1-s2-hp",  # specify the action to use the S2 high-priority trigger
 }
 
@@ -14,7 +15,7 @@ message = json.dumps(payload)
 
 # Submit via HTTP webhook endpoint
 response = requests.post(
-    "http://localhost:12000/samples",
+    "http://localhost:12001/samples",
     data=message,
     headers={"Content-Type": "application/json"},
 )
