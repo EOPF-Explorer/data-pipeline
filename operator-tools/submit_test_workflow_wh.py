47f6ee7a65f38181828475960aef73d0c5bf43a5
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2026-03-02 11:44:02 +0100

    Update Sentinel-2 L2A product processing scripts and dependencies
    - Modified `submit_test_workflow_wh.py` to use updated source URL and collection name.
    - Updated `submit_test_workflow_wh_list.py` with new Sentinel-2 L2A product IDs for processing.
    - Adjusted `pyproject.toml` to change dependencies, including specifying a version for `eopf` and using the main branch for `eopf-stac`.
    - Added new Jupyter notebook `eopf-cpm-production-sentinel-2-l2a-from-cdse.ipynb` for downloading and processing Sentinel-2 L2A products to CPM and uploading to S3.

--- operator-tools/submit_test_workflow_wh.py
+++ operator-tools/submit_test_workflow_wh.py
@@ -6,8 +6,8 @@ import requests
 # Test STAC item submission
 payload = {
     # "source_url": "https://stac.core.eopf.eodc.eu/collections/sentinel-2-l2a/items/S2C_MSIL2A_20251117T090251_N0511_R007_T35SMA_20251117T124014",
-    "source_url": "https://s3.explorer.eopf.copernicus.eu/esa-zarr-sentinel-explorer-fra/cpm-manual/S2A_MSIL2A_20210917T115221_N0500_R123_T28RBS_20230110T165456.json",
-    "collection": "sentinel-2-l2a-staging",
+    "source_url": "https://s3.explorer.eopf.copernicus.eu/esa-zarr-sentinel-explorer-fra/cpm-manual/S2C_MSIL2A_20250821T112131_N0511_R037_T29TPE_20250821T150517.json",
+    "collection": "sentinel-2-l2a",
     "action": "convert-v1-s2-hp",  # specify the action to use the S2 high-priority trigger
 }
 
@@ -15,7 +15,7 @@ message = json.dumps(payload)
 
 # Submit via HTTP webhook endpoint
 response = requests.post(
-    "http://localhost:12001/samples",
+    "http://localhost:12000/samples",
     data=message,
     headers={"Content-Type": "application/json"},
 )
